d3dbd1536ca10a7cfe8c194754424a41fe8cce8d
vinicius-luz-oliveira <78031432+vinicius-luz-oliveira@users.noreply.github.com>
2021-01-31 01:21:27 -0300

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -10,23 +10,8 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-/*   COMENTED OUT SO I CAN PUT WAL COLORS INSTEAD
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-*/
-
-
-/* static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-/*	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
-*/
+static const char *fonts[]          = { "monospace:size=3" };
+static const char dmenufont[]       = "monospace:size=3";
 #include "/home/vinitg/.cache/wal/colors-wal-dwm.h"
 /* tagging */
 static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
@@ -81,17 +66,20 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", sel_fg, "-nf", norm_fg, "-sb", sel_bg, "-sf", norm_bg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *ffcmd[]    = {"firefox", NULL};
 static const char *rangercmd[] = {"st", "-e", "ranger", NULL};
-
+static const char *discordcmd[]= {"discord", NULL};
+static const char *steamcmd[]= {"steam", NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = ffcmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = rangercmd} },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = discordcmd} },
+	{ MODKEY,                       XK_s,      spawn,          {.v = steamcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -129,6 +117,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[7]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
